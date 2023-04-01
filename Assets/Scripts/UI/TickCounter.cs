9ce859f0c7577431a86d480d203a420b9dc5f982
Axye <axye1024@gmail.com>
2023-04-01 19:49:08 +1100

    Remove log spam + fix not updating on start

--- Assets/Scripts/UI/TickCounter.cs
+++ Assets/Scripts/UI/TickCounter.cs
@@ -8,22 +8,22 @@ public class TickCounter : MonoBehaviour
     private int current = -1, max;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         ticks = transform.GetComponentsInChildren<Image>();
         if (ticks.Length == 0)
         {
             Debug.LogError("Could not find ticks");
-            //ticks = new Image[genAmount];
-            //for (int i = 0; i < genAmount; i++)
-            //{
-            //    GameObject obj = new("Tick" + i);        
-            //    obj.transform.parent = transform;
-            //    obj.transform.localPosition = offset * i;
-            //    Image im = obj.AddComponent<Image>();
-            //    im.sprite = full;
-            //    ticks[i] = im;
-            //}
+            /*ticks = new Image[genAmount];
+            for (int i = 0; i < genAmount; i++)
+            {
+                GameObject obj = new("Tick" + i);        
+                obj.transform.parent = transform;
+                obj.transform.localPosition = offset * i;
+                Image im = obj.AddComponent<Image>();
+                im.sprite = full;
+                ticks[i] = im;
+            }*/
         }
 
         max = ticks.Length;
@@ -34,10 +34,8 @@ public class TickCounter : MonoBehaviour
     // Update is called once per frame
     public void Set(int set)
     {
-        print("attempted to set  " + set);
         if (current != set)
         {
-            print("set to " + set);
             for (int i = 0; i < max; i++)
                 ticks[i].sprite = set > i ? full : empty;
             current = set;
