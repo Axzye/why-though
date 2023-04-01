225efaa426dae452b46fc1e6103541548edc3c6a
Axye <axye1024@gmail.com>
2023-04-01 19:48:31 +1100

    Add input popup for dialogue interactable

--- Assets/Scripts/Managers/NPC.cs
+++ Assets/Scripts/Managers/NPC.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class NPC : DialogueTrigger
 {
+    private SpriteRenderer indicator;
     private InputMaster input;
     private LayerMask playerLayer = 64;
 
     private void Awake()
     {
         input = new();
+        Sprite indS = Resources.Load<Sprite>("Sprites/UI/button_f");
+        GameObject indNew = new();
+        indNew.transform.parent = transform;
+        indNew.transform.localPosition = Vector2.up;
+        indicator = indNew.AddComponent<SpriteRenderer>();
+        indicator.sprite = indS;
     }
 
     private void OnEnable() => input.Player.Enable();
@@ -15,8 +22,10 @@ public class NPC : DialogueTrigger
 
     public void Update()
     {
+        indicator.enabled = false;
         if (Physics2D.OverlapCircle(transform.position, 0.75f, playerLayer))
         {
+            indicator.enabled = true;
             if (input.Player.Use.triggered)
             {
                 Trigger();
