3bed68cca7e3b7953efd3921bc708567a48be6d3
Axye <axye1024@gmail.com>
2023-07-07 20:44:21 +1000

    Temporary slider

--- Assets/Scripts/UI/UIHP.cs
+++ Assets/Scripts/UI/UIHP.cs
@@ -6,24 +6,28 @@ public class UIHP : MonoBehaviour
 {
     // boiler plate code!
     public TMP_Text textHp, textTp;
-    public TickCounter hpCounter, tpCounter;
+    public Slider hpSlider, tpSlider;
+    //public TickCounter hpCounter, tpCounter;
     public Image rainbow;
+    public Gradient rainbowGradient;
     private Party party;
 
     private void Start()
     {
         party = Party.Inst;
-
-        
     }
 
     private void Update()
     {
-        hpCounter.Set(party.hp);
-        tpCounter.Set(party.tp);
+        //hpCounter.Set(party.hp);
+        //tpCounter.Set(party.tp);
+        hpSlider.value = party.hp;
+        tpSlider.value = party.tp;
+        hpSlider.maxValue = party.hp.GetMax;
+        tpSlider.maxValue = party.tp.GetMax;
         textHp.text = party.hp.ToString("00") + '/' + party.hp.GetMax.ToString("00");
         textTp.text = party.tp.ToString("00") + '/' + party.tp.GetMax.ToString("00");
 
-        //rainbow.color = Color.HSVToRGB(Time.time * 0.25f % 1f, 0.6f, 1f);
+        rainbow.color = Color.HSVToRGB(Time.time * 0.2f % 1f, 0.5f, 1f);
     }
 }
